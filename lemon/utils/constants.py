5df2acc857a6ebec581e6b1ba057c18867c06249
Crazy Cookie <mr.tmk_0210@mail.ru>
2021-04-19 01:52:37 +0600

    init

---
+++ lemon/utils/constants.py
@@ -0,0 +1,30 @@
+USER_ROLE_SUPER_USER = 1
+USER_ROLE_CUSTOMER = 2
+USER_ROLE_MANAGER = 3
+USER_ROLES = (
+    (USER_ROLE_SUPER_USER, 'admin'),
+    (USER_ROLE_CUSTOMER, 'customer'),
+    (USER_ROLE_MANAGER, 'manager'),
+)
+
+CATEGORY_TYPE_CLOTHES = 1
+CATEGORY_TYPE_SHOES = 2
+CATEGORY_TYPE_ACCESSORIES = 3
+CATEGORY_TYPES = (
+    (CATEGORY_TYPE_CLOTHES, 'clothes'),
+    (CATEGORY_TYPE_SHOES, 'shoes'),
+    (CATEGORY_TYPE_ACCESSORIES, 'accessories'),
+)
+
+MEN_PRODUCT = 'M'
+WOMEN_PRODUCT = 'W'
+GIRLS_PRODUCT = 'G'
+BOYS_PRODUCT = 'B'
+NEWBORNS_PRODUCT = 'N'
+PRODUCT_TYPE = (
+    (MEN_PRODUCT, 'Мужской'),
+    (WOMEN_PRODUCT, 'Женский'),
+    (GIRLS_PRODUCT, 'Для девочек'),
+    (BOYS_PRODUCT, 'Для мальчиков'),
+    (NEWBORNS_PRODUCT, 'Для новорожденных'),
+)
