4bd27565e00159954270fa99b005ebdce01fb76d
Crazy Cookie <mr.tmk_0210@mail.ru>
2021-05-16 11:58:30 +0600

    models registered to admin site

--- lemon/auth_/admin.py
+++ lemon/auth_/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
 
-# Register your models here.
+from auth_ import models
+
+admin.site.register(models.MainUser)
+admin.site.register(models.UserProfile)
--- lemon/core/admin.py
+++ lemon/core/admin.py
@@ -1,3 +1,42 @@
 from django.contrib import admin
+from mptt.admin import MPTTModelAdmin
 
-# Register your models here.
+from core.models import Product, ProductImage, ProductSpecification, Category, Brand, City, Store, Promotion
+
+
+class ProductImageInLine(admin.TabularInline):
+    model = ProductImage
+
+
+class ProductSpecificationInLine(admin.TabularInline):
+    model = ProductSpecification
+
+
+class CustomMPTTModelAdmin(MPTTModelAdmin):
+    # specify pixel amount for this ModelAdmin only:
+    mptt_level_indent = 20
+
+
+@admin.register(Product)
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ('id', 'name', 'category', 'type')
+    inlines = (ProductImageInLine, ProductSpecificationInLine)
+
+
+@admin.register(Brand)
+class BrandAdmin(admin.ModelAdmin):
+    list_display = ('name', )
+
+
+@admin.register(City)
+class CityAdmin(admin.ModelAdmin):
+    list_display = ('name', )
+
+
+@admin.register(Store)
+class StoreAdmin(admin.ModelAdmin):
+    list_display = ('name', 'brand')
+
+
+admin.site.register(Category, CustomMPTTModelAdmin)
+admin.site.register(Promotion)
